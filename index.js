7bb9449fd1315ed6c8cb80b6911b67aec6cbae39
lera-ro <lera.rodoman@yandex.ru>
2024-07-21 15:17:13 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+gsap.from(".box1", {delay: 1, duration:2.5, ease: "elastic.out", x: 250})
+gsap.from(".box2", {opacity: 0, delay: 2.5, duration:2.5, ease: "elastic.out", x: 250})
+gsap.from(".box3", {opacity: 0, delay: 2.5, duration:2.5, ease: "elastic.out", x: 250})
+gsap.from(".box4", {opacity: 0, delay: 2.5, duration:2.5, ease: "elastic.out", x: 250})
+gsap.from(".item", {y: -500, opacity: 0, delay: 3.5, duration: 1, stagger: .2})
+gsap.from(".btn-img-one", {x: 500, ease: "power3.in", duration: 2.5, delay: 4, opacity: 0})
+gsap.from(".btn-img-two", {x: -500, ease: "power3.in", duration: 2.5, delay: 4, opacity: 0})
+gsap.from(".par", {y: -500, opacity: 0, delay: 5, duration: 1, stagger: .2})
+
